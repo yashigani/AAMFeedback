58aa856de97f791c7941a6365d971575c14bcb99
azu <azuciao@gmail.com>
2012-12-13 17:58:20 +0900

    use UIDeviceIdentifier

--- AAMFeedback/AAMFeedback/AAMFeedbackViewController.m
+++ AAMFeedback/AAMFeedback/AAMFeedbackViewController.m
@@ -8,6 +8,7 @@
 
 #import "AAMFeedbackViewController.h"
 #import "AAMFeedbackTopicsViewController.h"
+#import "UIDeviceHardware.h"
 #include <sys/sysctl.h>
 
 @interface AAMFeedbackViewController(private)
@@ -387,49 +388,10 @@
     return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
 }
 
-// Codes are from 
-// http://stackoverflow.com/questions/448162/determine-device-iphone-ipod-touch-with-iphone-sdk
-// Thanks for sss and UIBuilder
-- (NSString *) _platform
-{
-    int mib[2];
-    size_t len;
-    char *machine;
-    
-    mib[0] = CTL_HW;
-    mib[1] = HW_MACHINE;
-    sysctl(mib, 2, NULL, &len, NULL, 0);
-    machine = malloc(len);
-    sysctl(mib, 2, machine, &len, NULL, 0);
-    
-    NSString *platform = [NSString stringWithCString:machine encoding:NSASCIIStringEncoding];
-    free(machine);
-    return platform;
-}
 
 - (NSString *) _platformString
 {
-    NSString *platform = [self _platform];
-    if ([platform isEqualToString:@"iPhone1,1"]) return @"iPhone 1G";
-    if ([platform isEqualToString:@"iPhone1,2"]) return @"iPhone 3G";
-    if ([platform isEqualToString:@"iPhone2,1"]) return @"iPhone 3GS";
-    if ([platform isEqualToString:@"iPhone3,1"]) return @"iPhone 4";
-    if ([platform isEqualToString:@"iPhone4,1"]) return @"iPhone 4S";
-    if ([platform isEqualToString:@"iPhone5,1"]) return @"iPhone 5";
-    if ([platform isEqualToString:@"iPod1,1"])   return @"iPod Touch 1G";
-    if ([platform isEqualToString:@"iPod2,1"])   return @"iPod Touch 2G";
-    if ([platform isEqualToString:@"iPod3,1"])   return @"iPod Touch 3G";
-    if ([platform isEqualToString:@"iPod4,1"])   return @"iPod Touch 4G";
-    if ([platform isEqualToString:@"iPad1,1"])   return @"iPad WiFi";
-    if ([platform isEqualToString:@"iPad2,1"])   return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,2"])   return @"iPad 2 (GSM)";
-    if ([platform isEqualToString:@"iPad2,3"])   return @"iPad 2 (CDMA)";
-    if ([platform isEqualToString:@"iPad3,1"])   return @"iPad 3 (Wi-Fi)";
-    if ([platform isEqualToString:@"iPad3,2"])   return @"iPad 3 (GSM)";
-    if ([platform isEqualToString:@"iPad3,3"])   return @"iPad 3 (CDMA)";
-    if ([platform isEqualToString:@"i386"])   return @"iPhone Simulator";
-    if ([platform isEqualToString:@"x86_64"])   return @"iPhone Simulator";
-    return platform;
+    return [UIDeviceHardware platformString];
 }
 
 @end
